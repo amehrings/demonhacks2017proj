7ae4075c437053f9f2509772af430879922ade85
Sam Gilroy <sam.gilroy2@gmail.com>
2017-10-28 12:00:36 -0500

    trying to add a timer
    Signed-off-by: Sam Gilroy <sam.gilroy2@gmail.com>

--- app/src/main/java/com/example/sam/safedrinkingapp/BAC.java
+++ app/src/main/java/com/example/sam/safedrinkingapp/BAC.java
@@ -1,5 +1,6 @@
 package com.example.sam.safedrinkingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,30 +16,35 @@ public class BAC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bac);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+                                   @Override
+                                   public void onClick(View view) {
+                                       startAIDY();
+                                   }
+                               });
 
-        TextView textView6 = (TextView)findViewById(R.id.textView6);
+            TextView textView6 = (TextView)findViewById(R.id.textView6);
         textView6.setText(String.format("%.2f", MainActivity.drunkCalc));
 
-        TextView textView7 = (TextView)findViewById(R.id.textView7);
+            TextView textView7 = (TextView)findViewById(R.id.textView7);
         textView7.setText("Hard Liquor (Shot): " + String.format("%.2f", MainActivity.hardLiquorShot));
 
-        TextView textView8 = (TextView)findViewById(R.id.textView8);
+            TextView textView8 = (TextView)findViewById(R.id.textView8);
         textView8.setText("Beer (12oz. @ 5%): " + String.format("%.2f", MainActivity.beer));
 
-        TextView textView9 = (TextView)findViewById(R.id.textView9);
+            TextView textView9 = (TextView)findViewById(R.id.textView9);
         textView9.setText("Wine Bottle (25 oz. @ 12%): " + String.format("%.2f",MainActivity.wine));
 
+        }
+
+    public void startAIDY() {
+        Intent intent;
+        intent = new Intent(this, AmIDrunkYet.class);
+        startActivity(intent);
     }
 
 }
